f29ca8581e2313edd91402bf532c5dddbe828559
Dylan Sieren <dylan.sieren01@gmail.com>
2019-10-30 01:44:35 -0500

    want to make sure the node.js app will print to the heroku screen so i see it is live and connected

--- server/server.js
+++ server/server.js
@@ -1,18 +1,26 @@
-//server.js
-const express = require('express');
-const favicon = require('express-favicon');
-const path = require('path');
-const port = process.env.PORT || 8080;
-const app = express();
+// //server.js
+// const express = require('express');
+// const favicon = require('express-favicon');
+// const path = require('path');
+// const port = process.env.PORT || 8080;
+// const app = express();
 
-app.use(favicon(__dirname + '../build/favicon.ico'));
-// the __dirname is the current directory from where the script is running
-app.use(express.static(__dirname));
-app.use(express.static(path.join(__dirname, '../build')));
-app.get('/ping', function (req, res) {
- return res.send('pong');
+// app.use(favicon(__dirname + '../build/favicon.ico'));
+// // the __dirname is the current directory from where the script is running
+// app.use(express.static(__dirname));
+// app.use(express.static(path.join(__dirname, '../build')));
+// app.get('/ping', function (req, res) {
+//  return res.send('pong');
+// });
+// app.get('/*', function (req, res) {
+//   res.sendFile(path.join(__dirname, '../build', 'index.html'));
+// });
+// app.listen(port); 
+var express = require('express');
+var app = express();
+app.get('/', function (req, res) {
+  res.send('Hello World!');
 });
-app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, '../build', 'index.html'));
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
 });
-app.listen(port); 

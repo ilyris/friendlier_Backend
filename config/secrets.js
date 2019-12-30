5ebeb88b12155dbd5712d950ff1243a771f9406f
Dylan Sieren <dylan.sieren01@gmail.com>
2019-12-29 23:57:12 -0600

    removed a env var and placed it in the proper platform

--- config/secrets.js
+++ config/secrets.js
@@ -1,6 +1,5 @@
 const port = process.env.PORT || '8080';
 const seedPWTest = process.env.SEED_PW_TEST;
-const dbName = process.env.DB_NAME;
 const secret = process.env.JWT_SECRET || 'sdfgsdfgsd~!@`21123asdf~!@#';
 
 module.exports = {

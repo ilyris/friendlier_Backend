ffb3342b68615f1bfc83803a589040e7f92ea628
Dylan Sieren <dylan.sieren01@gmail.com>
2020-03-04 00:30:32 -0600

    added a space

--- data/migrations/20200225211856_messages.js
+++ data/migrations/20200225211856_messages.js
@@ -7,6 +7,7 @@ knex.schema.createTable("messages", tbl => {
   tbl.string("message").notNullable();
   tbl.bigInteger("sentAt").notNullable();
   tbl.timestamps(true, true);
+  
 });
 
 exports.down = knex => knex.schema.dropTableIfExists("messages");

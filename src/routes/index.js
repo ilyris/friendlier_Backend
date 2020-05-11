f4d2933131de560a905cafda02ce4d6f315671ca
Dylan Sieren <dylan.sieren01@gmail.com>
2020-05-11 10:46:21 -0500

    removed some logging

--- src/routes/index.js
+++ src/routes/index.js
@@ -232,13 +232,11 @@ function generateToken(user) {
 function authenticateToken(req, res, next) {
     // create a variable for the token from the clients request.
     const token = req.headers.authorization;
-
     // if token is false, return a 401.
     if (!token) return res.status(422).send("Access Denied")
     try {
         // Verify the JWT that we have to the clients JWT
         const verified = jwt.verify(token, JWT_SECRET)
-
         // store the verified payload to the user object in the locals object.
         res.locals.user = verified
         next()

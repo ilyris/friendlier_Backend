3ef5427fb0f1d7d22c76401301ce1c1ff9b38d18
Dylan <dylan.sieren01@gmail.com>
2020-11-19 21:10:04 -0600

    we'll get there

--- src/index.js
+++ src/index.js
@@ -1,19 +1,25 @@
 // ************* This is the main server file  ***********
 const express = require("express")
+const http = require('http')
 const cors = require("cors")
 const { PORT } = require("./config")
 const apiRouter = require("./routes")
+const socketio = require("socket.io")
 
-const server = express()
-const io = require("socket.io")(server)
+const app = express()
+const server = http.createServer(app)
+const io = socketio(server)
 
-server.use(cors())
-
-server.use(express.json()) // use middleware to parse the request body to a JSON object so we can access the data.
-server.use("/", apiRouter) // TRhis can be split into a ton of sub-routes
+app.use(cors())
+app.use(express.json()) // use middleware to parse the request body to a JSON object so we can access the data.
+// Run when a client connects
+app.use("/", apiRouter) // TRhis can be split into a ton of sub-routes
 io.on("connection", (socket) => {
     console.log("connect")
+
+    socket.emit('message', 'Welcome To the chat')
+    socket.broadcast.emit()
 })
-server.listen(PORT, () => {
+app.listen(PORT, () => {
     console.log(`Hideir REST API listening @ http://localhost:${PORT}`)
 })

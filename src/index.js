37078a2223d5d238511d87b895d4b487b099a87f
Dylan <dylan.sieren01@gmail.com>
2020-11-19 21:01:39 -0600

    tree shook to remove so much noise when trying to learn socket.io

--- src/index.js
+++ src/index.js
@@ -1,30 +1,20 @@
 // ************* This is the main server file  ***********
 const express = require("express")
 const cors = require("cors")
-// const whitelist = ["https://hideir.netlify.app"]
-// const corsOptions = {
-//     credentials: true,
-//     origin: (origin, callback) => {
-//         if (whitelist.includes(origin)) return callback(null, true)
-
-//         callback(new Error("Not allowed by CORS"))
-//     }
-// }
-
 const { PORT } = require("./config")
 const apiRouter = require("./routes")
-const messagesRouter = require("./routes/messages")
 
 const server = express()
+const httpServer = require('http').createServer(server)
+const io = require('socket.io')(httpServer)
 
-// server.use(cors(corsOptions))
+httpServer.use(cors())
 
-server.use(express.json()) // use middleware to parse the request body to a JSON object so we can access the data.
-server.use("/", apiRouter)
-server.use(messagesRouter)
-server.listen(PORT, () => {
+httpServer.use(express.json()) // use middleware to parse the request body to a JSON object so we can access the data.
+httpServer.use("/", apiRouter) // TRhis can be split into a ton of sub-routes
+io.on('connection', socket => {
+    console.log('connect');
+  });
+httpServer.listen(PORT, () => {
     console.log(`Hideir REST API listening @ http://localhost:${PORT}`)
 })
-const io = require("socket.io").listen(server)
-// io.listen(port)
-server.locals.io = io
--- src/routes/messages.js
+++ src/routes/messages.js
@@ -1,15 +1,15 @@
-const { Router } = require("express")
-const router = Router()
+// const { Router } = require("express")
+// const router = Router()
 
-// const jwt = require("jsonwebtoken")
-// const { hashSync, compareSync } = require("bcryptjs") // bcrypt will encrypt passwords to be saved in db
-// const { JWT_SECRET } = require("../config.js")
+// // const jwt = require("jsonwebtoken")
+// // const { hashSync, compareSync } = require("bcryptjs") // bcrypt will encrypt passwords to be saved in db
+// // const { JWT_SECRET } = require("../config.js")
 
-router.get("/messages", (req, res, next) => {
-    console.log(req.app.locals.io) //io object
-    const io = req.app.locals.io
-    io.emit("my event", { my: "data" }) //emit to everyone
-    res.send("OK")
-})
+// router.get("/messages", (req, res, next) => {
+//     console.log(req.app.locals.io) //io object
+//     const io = req.app.locals.io
+//     io.emit("my event", { my: "data" }) //emit to everyone
+//     res.send("OK")
+// })
 
-module.exports = router
+// module.exports = router

dcc8bcbb8d7b9f84fbe1cb271e63bad3d5288d39
Dylan <dylan.sieren01@gmail.com>
2020-11-23 11:52:53 -0600

    change back to 8081

--- src/routes/messages.js
+++ src/routes/messages.js
@@ -1,21 +1,18 @@
 const { PORT } = require("../config")
-var express = require("express")
-var http = require("https")
+var app = require("express")()
 var router = express.Router()
-var socketio = require("socket.io")
-
-var app = express()
-var server = http.Server(app)
-var websocket = socketio(server, {
+const server = require('https').createServer(app);
+const io = require('socket.io')(server, {
     cors: {
         origin: "https://hideir.netlify.app",
         methods: ["GET", "POST"]
     }
-})
-server.listen(PORT, () => console.log(`Socket.io listening on ${PORT}`))
+});
+
+server.listen(8081, () => console.log(`Socket.io listening on ${8081}`))
 
 // The event will be called when a client is connected.
-websocket.on("connection", (socket) => {
+io.on("connection", (socket) => {
     console.log("connection")
     socket.emit("hello", { data: "more data" })
 

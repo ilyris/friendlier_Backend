73c07c99e3831396b613ec11828846f04280aa1f
Dylan <dylan.sieren01@gmail.com>
2020-11-23 00:49:08 -0600

    added in the specific origin

--- src/routes/messages.js
+++ src/routes/messages.js
@@ -9,11 +9,11 @@ var websocket = socketio(server, {
     handlePreflightRequest: (req, res) => {
         const headers = {
             "Access-Control-Allow-Headers": "Content-Type, Authorization",
-            "Access-Control-Allow-Origin": req.headers.origin, //or the specific origin you want to give access to,
+            "Access-Control-Allow-Origin": "https://hideir.netlify.app", //or the specific origin you want to give access to,
             "Access-Control-Allow-Credentials": true
-        };
-        res.writeHead(200, headers);
-        res.end();
+        }
+        res.writeHead(200, headers)
+        res.end()
     }
 })
 server.listen(8081, () => console.log("Socket.io listening on *:8081"))

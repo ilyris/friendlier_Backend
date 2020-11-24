27da177c515517dc1efe857c04cd0496a70d497c
Dylan <dylan.sieren01@gmail.com>
2020-11-23 22:37:30 -0600

    starting from square one, moving everything to index and keeping it simple

--- src/index.js
+++ src/index.js
@@ -1,21 +1,29 @@
 // ************* This is the main server file  ***********
 const express = require("express")
-// const http = require("http")
 const cors = require("cors")
 const { PORT } = require("./config")
 const apiRouter = require("./routes")
 const app = express()
-// const server = http.createServer(app)
-// const socketio = require("socket.io").listen(server)
+const http = require('http')
+const socketio = require('socket.io')
+const server = http.createServer(app)
+const websocket = socketio(server)
 
-const messagesRouter = require("./routes/messages")
+// const messagesRouter = require('./routes/messages')
 
 app.use(cors())
 app.use(express.json()) // use middleware to parse the request body to a JSON object so we can access the data.
 app.use("/", apiRouter) // This can be split into a ton of sub-routes
 
-// Socket IO connection on port 8081
-app.use("/messages", messagesRouter)
+// // The event will be called when a client is connected.
+websocket.on('connection', socket => {
+    console.log('connection');
+    socket.emit('hello', {data: 'more data'});
+
+    socket.on('disconnect', () => {
+        console.log('user left')
+    })
+})
 
 app.listen(PORT, () => {
     console.log(`Hideir REST API listening @ http://localhost:${PORT}`)
--- src/routes/messages.js
+++ src/routes/messages.js
@@ -1,25 +1,29 @@
-const { PORT } = require("../config")
-var express = require("express")
-var app = express()
-var router = express.Router()
-const server = require("https").createServer(app)
-const io = require("socket.io")(server, {
-    cors: {
-        origin: "https://hideir.netlify.app",
-        methods: ["GET", "POST"]
-    }
-})
+// var express = require('express');
+// var http = require('http');
+// var router = express.Router();
+// var socketio = require('socket.io');
 
-server.listen(8081, () => console.log(`Socket.io listening on ${8081}`))
+// var app = express();
+// var server = http.createServer(app);
+// var websocket = socketio(server,   {
+//     cors: {
+//       origin: "http://localhost:3000",
+//       methods: ["GET", "POST"],
+//       allowedHeaders: ["my-custom-header"],
+//       credentials: true
+//     }
+//   });
 
-// The event will be called when a client is connected.
-io.on("connection", (socket) => {
-    console.log("connection")
-    socket.emit("hello", { data: "more data" })
+// // The event will be called when a client is connected.
+// websocket.on('connection', socket => {
+//     console.log('connection');
+//     socket.emit('hello', {data: 'more data'});
 
-    socket.on("disconnect", () => {
-        console.log("user left")
-    })
-})
+//     socket.on('disconnect', () => {
+//         console.log('user left')
+//     })
+// })
 
-module.exports = router
+// server.listen(8081, () => console.log('Socket.io listening on *:8081'));
+// console.log("we're running?")
+// module.exports = router;

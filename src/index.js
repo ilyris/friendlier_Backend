1c5829ebe178b446406183ad0063b6ae158b18fc
Dylan <dylan.sieren01@gmail.com>
2020-11-24 10:25:49 -0600

    socket.io connection works locally at the moment, The reason it wasnt working before was because the package version didn't meet the same version as the package on the server

--- src/index.js
+++ src/index.js
@@ -3,29 +3,16 @@ const express = require("express")
 const cors = require("cors")
 const { PORT } = require("./config")
 const apiRouter = require("./routes")
-const app = express()
-const http = require("http")
-const socketio = require("socket.io")
-const server = http.createServer(app)
-const websocket = socketio(server)
 
-// const messagesRouter = require('./routes/messages')
+const app = express() 
 
-app.use(cors())
+const messagesRouter = require('./routes/messages')
+// app.use(cors())
+app.use(cors());
 app.use(express.json()) // use middleware to parse the request body to a JSON object so we can access the data.
 app.use("/", apiRouter) // This can be split into a ton of sub-routes
-
-// // The event will be called when a client is connected.
-websocket.on("connection", (socket) => {
-    console.log("connection")
-    socket.emit("hello", { data: "more data" })
-
-    socket.on("disconnect", () => {
-        console.log("user left")
-    })
-})
+app.use('/', messagesRouter)
 
 app.listen(PORT, () => {
     console.log(`Hideir REST API listening @ http://localhost:${PORT}`)
 })
-server.listen(8081);
--- src/routes/messages.js
+++ src/routes/messages.js
@@ -1,29 +1,30 @@
-// var express = require('express');
-// var http = require('http');
-// var router = express.Router();
-// var socketio = require('socket.io');
+const express = require('express');
+const socketIO = require("socket.io");
+const http = require('http');
 
-// var app = express();
-// var server = http.createServer(app);
-// var websocket = socketio(server,   {
-//     cors: {
-//       origin: "http://localhost:3000",
-//       methods: ["GET", "POST"],
-//       allowedHeaders: ["my-custom-header"],
-//       credentials: true
-//     }
-//   });
+let app = express();
+let router = express.Router();
+let server = http.createServer(app);
 
-// // The event will be called when a client is connected.
-// websocket.on('connection', socket => {
-//     console.log('connection');
-//     socket.emit('hello', {data: 'more data'});
+// The event will be called when a client is connected.
+let io = socketIO(server, {
+    cors: {
+      origin: "http://localhost:3000",
+      methods: ["GET", "POST"],
+      allowedHeaders: ["my-custom-header"],
+      credentials: true
+    }
+  })
 
-//     socket.on('disconnect', () => {
-//         console.log('user left')
-//     })
-// })
+io.on("connection", socket => {
+    console.log("connection")
+    socket.emit("hello", { data: "more data" })
 
-// server.listen(8081, () => console.log('Socket.io listening on *:8081'));
-// console.log("we're running?")
-// module.exports = router;
+    socket.on("disconnect", () => {
+        console.log("user left")
+    })
+})
+
+server.listen(8081, () => console.log('Socket.io listening on *:8081'));
+
+module.exports = router;
